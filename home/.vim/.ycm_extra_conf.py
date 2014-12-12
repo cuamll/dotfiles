3e5239ee02265f2fd5603fca660895449f79ced3
callumgray <callum.gray.10@ucl.ac.uk>
2014-12-12 15:56:28 +0000

    Updated .ycm_extra_conf.py to find C standard libraries; changed YCM settings in.vimrc

--- home/.vim/.ycm_extra_conf.py
+++ home/.vim/.ycm_extra_conf.py
@@ -9,7 +9,6 @@ flags = [
     '-Wall',
     '-Wextra',
     '-Werror',
-    '-Wc++98-compat',
     '-Wno-long-long',
     '-Wno-variadic-macros',
     '-fexceptions',
@@ -27,16 +26,17 @@ flags = [
     '-x', 'c++',
     # This path will only work on OS X, but extra paths that don't exist are not
     # harmful
-    '-isystem', '/System/Library/Frameworks/Python.framework/Headers',
-    '-isystem', '/usr/local/include',
-    '-isystem', '/usr/local/include/eigen3',
+    '-isystem', '/Library/Developer/CommandLineTools/usr/include/c++/v1',
     '-isystem', '/usr/include',
-    '-isystem', '/System/Library/Frameworks',
-    '-isystem', '/Library/Frameworks',
-    '-isystem', '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin',
     '-isystem', '/opt/local/bin',
     '-isystem', '/opt/local/include',
     '-isystem', '/opt/local/sbin',
+    '-isystem', '/System/Library/Frameworks/Python.framework/Headers',
+    '-isystem', '/usr/local/include',
+    '-isystem', '/usr/local/include/eigen3',
+    '-isystem', '/System/Library/Frameworks',
+    '-isystem', '/Library/Frameworks',
+    '-isystem', '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin',
     '-I', 'include'
     '-I.'
 ]
