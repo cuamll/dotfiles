803206cb8f81b001dd311628f36145f500a50c2a
Callum Gray <callum.gray.10@ucl.ac.uk>
2017-04-23 21:38:56 +0100

    initial void commit

--- home/.startup.sh
+++ home/.startup.sh
@@ -1,6 +1,7 @@
-#!/bin/zsh
-rfkill block bluetooth
+#!/bin/bash
 git config --global credential.helper cache
 # syndaemon -i 1 -K -d
-conky &
+conky -c ~/.conky/conkyrc1 &
+conky -c ~/.conky/conkyrc2 &
+conky -c ~/.conky/conkyrc3 &
 exit
